18d47167f416a00b1236ba47fea39daa05937a74
Vítor Caruso <crfvitor8d@hotmail.com>
2025-12-01 22:59:12 -0300

    day 1: part one

---
+++ 1secret_entrance/secret_entrance.go
@@ -0,0 +1,63 @@
+package secretentrance
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"strconv"
+)
+
+func openFile(path string) (io.Reader, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	return file, nil
+}
+
+func extractSlice(rd io.Reader) []string {
+	scanner := bufio.NewScanner(rd)
+	lines := make([]string, 0, 512)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines
+}
+
+func countZeroReset(rotations []string, start int) (int, error) {
+	zeroCount := 0
+	acc := start
+	for _, rotation := range rotations {
+		direction := rotation[0]
+		valueStr := rotation[1:]
+
+		value, err := strconv.Atoi(valueStr)
+		if err != nil {
+			return -1, err
+		}
+
+		if direction == 'L' {
+			value = -value
+		}
+		acc = (acc + value) % 100
+		if acc == 0 {
+			zeroCount++
+		}
+	}
+	return zeroCount, nil
+}
+
+func PartOne() int {
+	file, err := openFile("1secret_entrance/input.txt")
+	if err != nil {
+		panic(err)
+	}
+	input := extractSlice(file)
+
+	res, err := countZeroReset(input, 50)
+	if err != nil {
+		panic(err)
+	}
+
+	return res
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	secretentrance "github.com/CarusoVitor/advent-of-code-2025/1secret_entrance"
+)
+
+func main() {
+	fmt.Println(secretentrance.PartOne())
+}

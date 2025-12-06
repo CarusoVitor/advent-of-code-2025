6ab2a0db8f1a6a90ab8ad11d4d89e2975c63712e
Vítor Caruso <vitor.caruso@aprix.com.br>
2025-12-06 18:27:33 -0300

    day 3: part two

--- 3lobby/lobby.go
+++ 3lobby/lobby.go
@@ -6,26 +6,36 @@ import (
 	filehandling "github.com/CarusoVitor/advent-of-code-2025/file_handling"
 )
 
-func getJoltage(bank string) (int, error) {
-	var left byte = 0
-	var right byte = 0
+func getJoltage(bank string, size int) (int, error) {
+	joltage := make([]byte, size)
+	nextIdx := 0
 
-	for i := 0; i < len(bank); i++ {
-		num := bank[i]
-		if num > left && i < len(bank)-1 {
-			left = num
-			right = bank[i+1]
-		} else if num > right {
-			right = num
+	for batteryIdx := 0; batteryIdx < len(bank); batteryIdx++ {
+		battery := bank[batteryIdx]
+		batteriesLeft := min(len(bank)-batteryIdx, size)
+		joltageIdx := size - batteriesLeft
+
+		for joltageIdx < nextIdx {
+			if battery > joltage[joltageIdx] {
+				joltage[joltageIdx] = battery
+				nextIdx = joltageIdx + 1
+				break
+			}
+			joltageIdx++
+		}
+		if joltageIdx == nextIdx && nextIdx < size {
+			joltage[nextIdx] = battery
+			nextIdx++
 		}
 	}
-	return strconv.Atoi(string([]byte{left, right}))
+
+	return strconv.Atoi(string(joltage))
 }
 
-func outputJoltage(batteries []string) int {
+func outputJoltage(batteries []string, size int) int {
 	joltage := 0
 	for _, bank := range batteries {
-		bankJoltage, err := getJoltage(bank)
+		bankJoltage, err := getJoltage(bank, size)
 
 		if err != nil {
 			panic(err)
@@ -41,5 +51,14 @@ func PartOne() int {
 		panic(err)
 	}
 	input := filehandling.ExtractSliceNewLine(file)
-	return outputJoltage(input)
+	return outputJoltage(input, 2)
+}
+
+func PartTwo() int {
+	file, err := filehandling.OpenFile("3lobby/input.txt")
+	if err != nil {
+		panic(err)
+	}
+	input := filehandling.ExtractSliceNewLine(file)
+	return outputJoltage(input, 12)
 }

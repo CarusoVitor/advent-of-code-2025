29350fffb1a24ed337a7f0e53fc8e88680f33903
Vítor Caruso <crfvitor8d@hotmail.com>
2025-12-20 16:51:59 -0300

    day 7: part two

--- 7laboratories/laboratories.go
+++ 7laboratories/laboratories.go
@@ -40,6 +40,60 @@ func totalSplits(grid [][]byte, start point) int {
 	return splits
 }
 
+// Each splitter causes an additional timeline to be created, since
+// you can interpet that the beam that got split went to one of the two
+// directions, and a new beam went to the other direction.
+// The only problem here is that if there are two beams separated by only
+// one empty space, the beams can stack. For example:
+//
+// .......S.......
+// .......|.......
+// .......^.......
+// .......|.......
+// .....|^X^|..... in X two beams are stacked
+//
+// This means that we have to keep track of stacked beams,
+// since they represent different timelines.
+// Here, this is done through the value (count) of the beams map, so,
+// everytime a split is found, we create a new timeline for each stacked beam,
+// deleting the unsplitted stacked beams
+func totalTimelines(grid [][]byte, start point) int {
+	beams := make(map[point]int, len(grid))
+	beams[start] = 1
+	timelines := 1
+	for yIdx := start.y + 1; yIdx < len(grid); yIdx++ {
+		for beamPoint, count := range beams {
+			if grid[yIdx][beamPoint.x] != '^' {
+				continue
+			}
+
+			left := point{x: beamPoint.x + 1, y: yIdx}
+			right := point{x: beamPoint.x - 1, y: yIdx}
+
+			beams[left] += count
+			beams[right] += count
+			timelines += count
+
+			delete(beams, beamPoint)
+		}
+	}
+	return timelines
+}
+
+func PartTwo() int {
+	file, err := filehandling.OpenFile("7laboratories/input.txt")
+	if err != nil {
+		panic(err)
+	}
+	grid := filehandling.ExtractGrid(file)
+
+	start := getStartingLocation(grid)
+	if start == (point{}) {
+		panic("invalid starting location")
+	}
+	return totalTimelines(grid, start)
+}
+
 func PartOne() int {
 	file, err := filehandling.OpenFile("7laboratories/input.txt")
 	if err != nil {
--- main.go
+++ main.go
@@ -7,5 +7,5 @@ import (
 )
 
 func main() {
-	fmt.Println(laboratories.PartOne())
+	fmt.Println(laboratories.PartTwo())
 }
